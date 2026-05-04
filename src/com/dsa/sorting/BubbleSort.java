ecef5c64ea481dd50948bbffdbe27ab5f86b6bbd
pravudatta10 <mrpravudatta10@gmail.com>
2026-05-04 23:33:51 +0530

    bubbe sort

--- src/com/dsa/sorting/BubbleSort.java
+++ src/com/dsa/sorting/BubbleSort.java
@@ -1,4 +1,25 @@
 package com.dsa.sorting;
 
+import java.util.Arrays;
+
 public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {31, 5, 7, 45, 60, 4};
+
+        new BubbleSort().bubbleSort(arr);
+    }
+
+    public void bubbleSort(int[] arr) {
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        Arrays.stream(arr).forEach(System.out::println);
+    }
 }
